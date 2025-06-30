5d0ca4aa1bffad87c043ac55d1762ce0b0a194fb
H0llyW00dzZ <h0llyw00dzz@pm.me>
2025-06-30 22:32:28 +0700

    Feat [CI/CD] GitHub Actions workflow for test coverage (#35)
    * Feat [CI/CD] GitHub Actions workflow for test coverage
    
    - [+] feat(coverage): add GitHub Actions workflow for test coverage
    - [+] feat(readme): add coverage badge to README
    - [+] feat(package): add coveralls script and dependency
    
    * Fix NPM CI
    
    - [+] fix(coverage.yml): change npm ci to npm install in workflow for dependency installation
    
    * Fix command coverage
    
    - [+] fix(coverage.yml): update test command from npm run test:coverage to npm run coveralls
    
    * Revert "Fix command coverage"
    
    This reverts commit 63bc680c596d24393ba8a0307b2163a83d160fbb.
    
    * Fix JS Syntax
    
    - [+] refactor(babel.config.js): change export syntax from ES module to CommonJS
    - [+] refactor(jest.config.js): change export syntax from ES module to CommonJS
    
    * Update NodeJS Version
    
    - [+] chore: update Node.js version in GitHub workflow and package files to 19.x
    
    * Update README.md
    
    - [+] fix(README.md): update coverage badge URL to remove branch specification

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,20 @@
-export const testEnvironment = 'node';
-export const testMatch = ['**/*.test.ts'];
-export const transform = {
-  '^.+\\.tsx?$': 'babel-jest'
-};
-export const projects = [
-  {
-    displayName: 'node',
-    testEnvironment: 'node',
-    // More specific testMatch to exclude browser tests
-    testMatch: ['<rootDir>/tests/*.test.ts', '<rootDir>/tests/!(browser)/**/*.test.ts'],
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.tsx?$': 'babel-jest'
   },
-  {
-    displayName: 'browser',
-    testEnvironment: 'jsdom',
-    testMatch: ['<rootDir>/tests/browser/**/*.test.ts'],
-  }
-];
+  projects: [
+    {
+      displayName: 'node',
+      testEnvironment: 'node',
+      // More specific testMatch to exclude browser tests
+      testMatch: ['<rootDir>/tests/*.test.ts', '<rootDir>/tests/!(browser)/**/*.test.ts'],
+    },
+    {
+      displayName: 'browser',
+      testEnvironment: 'jsdom',
+      testMatch: ['<rootDir>/tests/browser/**/*.test.ts'],
+    }
+  ]
+};
