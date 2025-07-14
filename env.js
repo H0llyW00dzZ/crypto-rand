e84e83995e75a0ce243cead87969c5d59efbd817
H0llyW00dzZ <h0llyw00dzz@pm.me>
2025-07-14 13:08:01 +0700

    bump

--- env.js
+++ env.js
@@ -1,3 +1,4 @@
 // Access the environment variable from process.env
 const isBrowser = process.env.BROWSER_ENV === "true";
 console.log(`Running in ${isBrowser ? "browser" : "nodejs"} environment.`);
+
