c5a93d64bc7f8c6ecfcdb1ea65ae34b2ab0ea29f
H0llyW00dzZ <h0llyw00dzz@pm.me>
2025-07-14 10:34:57 +0700

    Remove isTest Variable for now
    - [+] refactor(babel.config.js): remove unused isTest variable declaration

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,4 @@
 module.exports = (api) => {
-  // The 'isTest' variable is declared but not immediately used; it will be accessed when called.
-  const isTest = api.env("test");
   // Get browser environment from api.caller.
   const isBrowser = api.caller(
     (caller) => caller && caller.envName === "browser"
