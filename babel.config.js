614d51663ded9c8186d6ae82dae864000d2a1838
H0llyW00dzZ <h0llyw00dzz@pm.me>
2025-07-14 10:42:53 +0700

    Revert "Remove isTest Variable for now"
    This reverts commit c5a93d64bc7f8c6ecfcdb1ea65ae34b2ab0ea29f.

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,6 @@
 module.exports = (api) => {
+  // The 'isTest' variable is declared but not immediately used; it will be accessed when called.
+  const isTest = api.env("test");
   // Get browser environment from api.caller.
   const isBrowser = api.caller(
     (caller) => caller && caller.envName === "browser"
